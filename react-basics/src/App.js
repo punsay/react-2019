df6fe653b829d7b7f4047cee08bc86d3b5b1d4e9
punsay <punsay.prakhar@tothenew.com>
2019-09-07 19:06:55 +0530

    32 functional and stateful components

--- react-basics/src/App.js
+++ react-basics/src/App.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from  './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div>
+          <h1>Hi, I am react App!</h1>
+          <Person/>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
+
+    /* return React.createElement('div',{className:'App'},React.createElement('h1',null,'hi, I\'m a react app!')); */
   }
 }
 
