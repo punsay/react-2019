9135484168dffeec6894fb27547112093e103022
punsay <punsay.prakhar@othenew.com>
2019-09-09 11:26:32 +0530

    done

--- react-basics/src/App.js
+++ react-basics/src/App.js
@@ -3,18 +3,59 @@ import './App.css';
 import Person from  './Person/Person';
 
 class App extends Component {
+  state = {
+    persons : [
+      {name:"Priya", age:"21"},
+      {name:"Punsay", age:"22"},
+      {name:"Mona", age:"25"}
+    ]
+  }
+
+  switchNameHandler = () => {
+    this.setState({
+      persons : [
+        {name:"Sameera", age:25},
+        {name:"Kiran", age:25},
+        {name:"Kiara", age:"25"}
+      ]
+    });
+  }
+
+  onChangedHandler = (event) => {
+    this.setState({
+      persons : [
+        {name:"Sameera", age:25},
+        {name:event.target.value, age:25},
+        {name:"Kiara", age:"25"}
+      ]
+    });
+  }
+
   render() {
-    return (
+    return(
       <div className="App">
         <div>
           <h1>Hi, I am react App!</h1>
-          <Person/>
+          <button onClick={this.switchNameHandler}>Switch Name</button>
+          <Person 
+            name={this.state.persons[0].name} 
+            age={this.state.persons[0].age} 
+            />
+          <Person 
+          name={this.state.persons[1].name}  
+          age={this.state.persons[1].age} 
+          changed={this.onChangedHandler} >
+            My Hobbies: Racing
+          </Person>
+          <Person 
+            name={this.state.persons[2].name}  
+            age={this.state.persons[2].age} 
+          />
         </div>
       </div>
     );
-
-    /* return React.createElement('div',{className:'App'},React.createElement('h1',null,'hi, I\'m a react app!')); */
+    /* TODO: if i am not using JSX below is the code */
+    /* return React.createElement('div',{className:'App'} ,React.createElement('h1',null,'hi, I\'m a react app!')); */
   }
 }
-
 export default App;
--- react-basics/src/Person/Person.js
+++ react-basics/src/Person/Person.js
@@ -1,8 +1,10 @@
 import React from 'react';
-const person = () => {
-    return <p>
-      I'm a person
-    </p>
+const person = (props) => {
+      return  <div>
+                  <p>I am {props.name} and {props.age} years ! {props.children}</p>
+                  <input onChange={props.changed} value={props.name} ></input>
+              </div>
+      /*  I'm a person and I am {Math.floor(Math.random()*30)} years old! */
 }
 
 export default person;
