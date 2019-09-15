99ab1a441334825f0745b1c3bdb3da8b3eb138eb
punsay <punsay.prakhar@tothenew.com>
2019-09-15 19:33:47 +0530

    basics person code added

--- react-basics/src/Person/Person.js
+++ react-basics/src/Person/Person.js
@@ -2,7 +2,7 @@ import React from 'react';
 const person = (props) => {
       return  <div>
                   <p>I am {props.name} and {props.age} years ! {props.children}</p>
-                  <input onChange={props.changed} value={props.name} ></input>
+                  <input onChange={props.changed} ></input>
               </div>
       /*  I'm a person and I am {Math.floor(Math.random()*30)} years old! */
 }
